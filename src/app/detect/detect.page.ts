1b08dca4606a9983e05f73ec3ca40119919b3e30
georgette-a <45202432+georgette-a@users.noreply.github.com>
2021-05-02 10:50:21 +0000

    audioInterface1

--- src/app/detect/detect.page.ts
+++ src/app/detect/detect.page.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Media, MediaObject } from "@ionic-native/media/ngx";
+import { File } from "@ionic-native/file/ngx";
+
 @Component({
   selector: 'app-detect',
   templateUrl: './detect.page.html',
   styleUrls: ['./detect.page.scss'],
 })
 export class DetectPage implements OnInit {
+  status: string="";
+  audioFile: MediaObject;
+  isRecording: boolean=false;
 
-  constructor() { }
+  constructor(private media: Media, private file:File) { }
 
   ngOnInit() {
   }
 
+  startRecording(){
+    this.audioFile = this.media.create(this.file.externalRootDirectory+'/audiofile.mp3');
+    this.audioFile.startRecord();
+    this.isRecording = true;
+    this.status ="recording...";
+  }
+
+  stopRecording(){
+    this.audioFile.stopRecord();
+    this.status ="done";
+    this.isRecording = false;
+  }
 }
