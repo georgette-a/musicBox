25bfd1a0665181f821e24797cffbf2b2d40e46b6
georgette-a <45202432+georgette-a@users.noreply.github.com>
2021-05-02 16:33:54 +0000

    auD

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { Media } from "@ionic-native/media/ngx";
+import { Media } from '@ionic-native/media/ngx';
 import { File } from "@ionic-native/file/ngx";
 
 @NgModule({
--- src/app/detect/detect.page.ts
+++ src/app/detect/detect.page.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Media, MediaObject } from "@ionic-native/media/ngx";
+import { Media, MediaObject } from '@ionic-native/media/ngx';
 import { File } from "@ionic-native/file/ngx";
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-detect',
@@ -13,21 +14,49 @@ export class DetectPage implements OnInit {
   audioFile: MediaObject;
   isRecording: boolean=false;
 
-  constructor(private media: Media, private file:File) { }
+  constructor(
+    private media: Media, 
+    private platform:Platform, 
+    private file: File) { }
 
   ngOnInit() {
   }
 
   startRecording(){
-    this.audioFile = this.media.create(this.file.externalRootDirectory+'/audiofile.mp3');
-    this.audioFile.startRecord();
-    this.isRecording = true;
-    this.status ="recording...";
+    alert(this.file.externalDataDirectory+ 'my_file.mp3');
+    this.file.createFile(this.file.externalDataDirectory, 'my_file.mp3', true).then(() => {
+      
+      
+      alert('hello4');
+      // this.audioFile = this.media.create(this.file.externalDataDirectory + 'my_file.m4a');
+      this.audioFile = this.media.create('/storage/emulated/0/Android/data/io.ionic.starter/files/my_file.mp3');
+      this.audioFile.onError.pipe().subscribe({
+        next: res => {
+          alert(JSON.stringify(res));
+        }
+      });
+      this.audioFile.onSuccess.pipe().subscribe({
+        next: res => {
+          alert("success");
+        }
+      });
+      alert('hello3');
+      this.audioFile.startRecord();
+      this.isRecording = true;
+
+      // alert('hello2');
+      // window.setTimeout(() => this.audioFile.stopRecord(), 5000);
+      // alert('hello');
+      
+    });
   }
 
   stopRecording(){
     this.audioFile.stopRecord();
-    this.status ="done";
+    this.audioFile.play
     this.isRecording = false;
+    alert(this.audioFile.getDuration());
+   
+
   }
 }
